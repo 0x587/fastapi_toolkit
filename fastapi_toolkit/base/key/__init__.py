99d082a301322f99a38b1ad88db753aac6488261
shawnnshao <shawnnshao@tencent.com>
2024-08-09 15:12:25 +0800

    2

--- fastapi_toolkit/base/key/__init__.py
+++
@@ -1,8 +0,0 @@
-from fastapi_toolkit.base.key.auth import Auth, AuthDBBackend
-from fastapi_toolkit.base.key.router import BaseRouter
-
-__all__ = [
-    "Auth",
-    "AuthDBBackend",
-    "BaseRouter",
-]
